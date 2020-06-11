bff8a661b9d03fdca3d9e6cdf2f881db0f35ec55
Alec Sands <sandsalecv@gmail.com>
2020-06-11 15:31:25 -0500

    another example with classes

--- greeter.js
+++ greeter.js
@@ -1,8 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = {
-    firstName: "Alec",
-    lastName: "Sands"
-};
+var user = new Student("Alec", "V", "Sands");
 document.body.textContent = greeter(user);
